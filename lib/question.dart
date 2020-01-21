aad2f2bf0eb15c1cb6d1dba50a4699207c04ecc6
Steve Lovett <stephendlovett@gmail.com>
2020-01-20 21:29:19 -0800

    Added container widget and styling for question text;

--- lib/question.dart
+++ lib/question.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
   final String questionText;
-    
-  Question(this.questionText); // positional argument - would be named argument if wrapped in curly braces.
-  
+
+  Question(
+      this.questionText); // positional argument - would be named argument if wrapped in curly braces.
+
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
